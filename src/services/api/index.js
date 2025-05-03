b5b1c99137e1c3a49a25e952c55cbdb9299cbbdc
gadekar123 <gadekarshivam111@gmail.com>
2025-05-03 17:19:44 +0530

    feat: Add API exports for reports, linesmen, dashboard, and auth services

---
+++ src/services/api/index.js
@@ -0,0 +1,23 @@
+import reportsApi from './reportsApi';
+import linesmenApi from './linesmenApi';
+import dashboardApi from './dashboardApi';
+import authApi from './authApi';
+import { apiRequest, BASE_URL } from './config';
+
+// Export individual APIs
+export {
+  reportsApi,
+  linesmenApi,
+  dashboardApi,
+  authApi,
+  apiRequest,
+  BASE_URL
+};
+
+// Default export of all APIs
+export default {
+  reports: reportsApi,
+  linesmen: linesmenApi,
+  dashboard: dashboardApi,
+  auth: authApi
+};
